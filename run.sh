5f3468b4a5f6aea889ed954d4c3e9cfbeb0d1468
Ricky Tran <rickydtran@ufl.edu>
2017-12-05 22:38:07 -0500

    new run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for i in 16 32 64 128 256 512 1024 2048
+for i in 16 32 64
 do
-  for j in 0.1 0.3 0.5 0.7 0.9
+  for j in 0.02 0.04 0.06 0.08 0.1 0.3 0.5 0.7 0.9
   do
-    bin/dijkstra 25 ${i} ${j} 255 > output.txt
+    zed_schedule.py bin/dijkstra 50 ${i} 0 ${j} 255 dijkstra_hw_wrapper.bit | tee metrics2/dijkstra_${i}_${j}.txt
   done
 done
